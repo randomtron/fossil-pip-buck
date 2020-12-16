1a940ea7eb52888749f1746dfcf784c6da65ad1e
randomtron <VladPenzik@yandex.ru>
2020-12-16 19:04:17 +0800

    changes: schema.js(fanfics pseudo db, fanfics query)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,23 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, 
+       GraphQLString, 
+       GraphQLSchema, 
+       GraphQLID,
+       GraphQLList
+    } = graphql;
+
+const fanfics = [
+    { id: '1', name: 'Fallout: Equestria', author: 'Kkat'},
+    { id: '2', name: 'Fallout Equestria: Project Horizon', author: 'Somber'},
+    { id: '3', name: 'Fallout Equestria: The Fossil', author: 'Lucky Ticket'},
+];
 
 
 const FanficType = new GraphQLObjectType({
     name: 'Fanfic',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         author: { type: GraphQLString },
     }),
@@ -17,16 +28,21 @@ const Query = new GraphQLObjectType({
     fields: {
         fanfic: {
             type: FanficType,
-            args: {id:{type: GraphQLString}},
+            args: {id:{type: GraphQLID}},
             resolve(parent, args) {
-    
-            }
+                return fanfics.find(fanfic => fanfic.id == args.id);
+            },
         },
-    }
+        fanfics: {
+            type: new GraphQLList(FanficType),
+            resolve(parent, args) {
+                return fanfics;
+            },
+        },
+    },
 });
 
 
 module.exports = new GraphQLSchema({
     query: Query,
 });
-
