5869333758e1465d0f6256fa1392e098601582ed
randomtron <VladPenzik@yandex.ru>
2020-12-25 21:28:07 +0800

    client: refactor: creater apollo config file, replaced ApolloProvider into the index.tsx

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 import { Header } from '../header/header';
 import { Routes } from '../../main/routes';
@@ -7,19 +7,12 @@ import { Footer } from '../footer/footer';
 
 import s from './app.module.css';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:3005/graphql',
-    cache: new InMemoryCache()
-});
-
 export const App = () => {
     return (
-        <ApolloProvider client={client}>
             <div className={s.appWrapper}>
                 <Header />
                 <Routes />
                 <Footer />
             </div>
-        </ApolloProvider>
     );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app/app';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+
+import {client} from './main/apollo';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
