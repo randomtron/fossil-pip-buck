ebb9a3e9058e10221e68c9a46d59ef62b304b3db
randomtron <VladPenzik@yandex.ru>
2021-01-12 17:51:14 +0800

    client: changes: Routes (added Switch and Redirect)

--- client/src/main/routes/index.tsx
+++ client/src/main/routes/index.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
-import {Stat} from '../../components/stat/stat';
-import {Data} from '../../components/data/data';
+import { Stat } from '../../components/stat/stat';
+import { Data } from '../../components/data/data';
 
-import {Characters} from '../../components/data/characters/characters';
-import {Locations} from '../../components/data/locations/locations';
+import { Characters } from '../../components/data/characters/characters';
+import { Locations } from '../../components/data/locations/locations';
 
-import {Special} from '../../components/stat/special/special';
-import {Perks} from '../../components/stat/perks/perks';
+import { Special } from '../../components/stat/special/special';
+import { Perks } from '../../components/stat/perks/perks';
 
 import s from './routes.module.css';
 
 export const Routes = () => {
     return (
         <div className={s.routes}>
-            <Route
-                path='/stat'
-                render={() => <Stat />} />
-            <Route
-                path='/data'
-                render={() => <Data />} />
+            <Switch>
+                <Redirect exact from='/' to='/stat' />
+                <Route
+                    path='/stat'
+                    render={() => <Stat />} />
+                <Route
+                    path='/data'
+                    render={() => <Data />} />
+            </Switch>
         </div>
-    )
+    );
 };
 
 export const SubNavRoutes = () => {
     return (
-    <div>
-        <Route exact path='/data' render={() => <Characters/>}/>
-        <Route path='/data/locations' render={() => <Locations/>}/>
+        <div>
+            <Route exact path='/data' render={() => <Characters />} />
+            <Route path='/data/locations' render={() => <Locations />} />
 
-        <Route path='/stat/special' render={() => <Special/>}/>
-        <Route path='/stat/perks' render={() => <Perks/>}/>
-    </div>)
+            <Route path='/stat/special' render={() => <Special />} />
+            <Route path='/stat/perks' render={() => <Perks />} />
+        </div>);
 };
